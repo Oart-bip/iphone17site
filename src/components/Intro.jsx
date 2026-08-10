7c3f8323749b26a1c2fd0e709c8551e62a7cea1c
Arthur <arthurnantestrento@gmail.com>
2026-08-10 20:46:02 -0300

    Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,14 +6,30 @@ function Intro () {
   { value: '48MP', label: 'Sistema de câmera', color: 'text-blue-600' },
   { value: '29h', label: 'Bateria', color: 'text-orange-500' }
 ];
- // label: texto 
+
+ // label: texto;
+ // mb: margens de todos os lados;
+ // md: redimensionamento para telas maiores;
+ // px, py..: padding de tal lado
+
+
 
  return (
-    <section> 
-        <div>
-            <h1>
+    <section className="bg-black py-20 px-6"> 
+        <div className="max-w-5xl mx-auto text-center">]
+            <h1 className=" text-6xl md:text-8xl font-bold mb-6"> 
                 Iphone 17 PRO
             </h1>
+            <p className="text-2xl md:text-4xl "></p>
+            <p> Titânio. Tão forte. Tão leve. Tão Pro.</p>
+            <p> Design mais refinado que já criamos. Titânio de grau aeroespacial. Chip A18 Pro. Sistema de câmera Pro revolucionário. </p>
+        </div>
+
+        <div>
+            <button> Compre Agora </button>
+            <button> Saiba mais </button>
+
+
         </div>
 
     </section>

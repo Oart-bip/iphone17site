27a95b9a99f2b62a040b9542fe0f1bc2a680bfdb
Arthur <arthurnantestrento@gmail.com>
2026-08-18 13:28:55 -0300

    hmm

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,20 +1,28 @@
 function Highlights() {
 
     return (
-        <section>
-            <div className="text-bol text-center ">
+        <section className="bg-black py-20 px-6" id="design">
+            <div>
+                <div className="max-w-7xl mx-auto ">
 
-                <h2 className="text-5xl">Design Revolucionário</h2>
+                    <h2 className="text-5xl">Design Revolucionário</h2>
+                    <p>Cada detalhe foi pensado para criar a melhor experiência.</p>
 
-                <p>Cada detalhe foi pensado para criar a melhor experiência.</p>
+                </div>
 
             </div>
 
             <div>
                 <div>
-                    <img src="../../public/img/titanium-design.jpg" alt="iphone-titanium"> </img>
-                    <h3> </h3>
-                    <p> </p>
+                    <img src="../../public/img/titanium-design (1).jpg" alt="iphone-titanium"> </img>
+                    <h3>Titânio Premium</h3>
+                    <p>Estrutura em titânio de grau aeroespacial. O smartphone mais forte e leve.</p>
+                </div>
+
+                <div>
+                    <img src="w-full rounded 2-xl mb-4../../public/img/ios-features (1).jpg" alt="ios 2025"></img>
+                    <h3 className="font-bold mb-2 text-3xl"> IOS 26 </h3>
+                    <p className="text-gray-300"> O sistema operacional mais avançado do mundo com IA integrada. </p>
                 </div>
             </div>
         </section>

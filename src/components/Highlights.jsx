74764d79aae60ba2b990e1a5a4d9acb72889f357
Arthur <arthurnantestrento@gmail.com>
2026-08-18 13:42:27 -0300

    teste

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,27 +2,25 @@ function Highlights() {
 
     return (
         <section className="bg-black py-20 px-6" id="design">
-            <div>
-                <div className="max-w-7xl mx-auto ">
+            <div className="max-w-7xl mx-auto">
 
-                    <h2 className="text-5xl">Design Revolucionário</h2>
-                    <p>Cada detalhe foi pensado para criar a melhor experiência.</p>
-
+                <div className="text-center mb-16">
+                    <h2 className="text-5xl font-bold mb-4">Design Revolucionário</h2>
+                    <p className=" text-xl text-gray-400">Cada detalhe foi pensado para criar a melhor experiência.</p>
                 </div>
-
-            </div>
-
-            <div>
-                <div>
-                    <img src="../../public/img/titanium-design (1).jpg" alt="iphone-titanium"> </img>
-                    <h3>Titânio Premium</h3>
-                    <p>Estrutura em titânio de grau aeroespacial. O smartphone mais forte e leve.</p>
+            
+            <div className="grid grid-cols-2 gap-8 mb-16">
+                <div className="bg-gray-900 rounded-3xl p-8">
+                    <img className=" w-full rounded-2xl mb-4"src="../../public/img/titanium-design (1).jpg" alt="iphone-titanium"> </img>
+                    <h3 className="font-bold mb-2 text-3xl">Titânio Premium</h3>
+                    <p className="text-gray-300">Estrutura em titânio de grau aeroespacial. O smartphone mais forte e leve.</p>
                 </div>
 
                 <div>
-                    <img src="w-full rounded 2-xl mb-4../../public/img/ios-features (1).jpg" alt="ios 2025"></img>
+                    <img className=" w-full rounded-2xl mb-4" src="../../public/img/ios-features (1).jpg" alt="ios 2025"></img>
                     <h3 className="font-bold mb-2 text-3xl"> IOS 26 </h3>
                     <p className="text-gray-300"> O sistema operacional mais avançado do mundo com IA integrada. </p>
+                </div>
                 </div>
             </div>
         </section>

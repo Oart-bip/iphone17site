c5c4d0e81b2fb71e3986c13a99e29a791c116abd
Arthur <arthurnantestrento@gmail.com>
2026-09-01 15:19:00 -0300

    Highlights

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -8,20 +8,45 @@ function Highlights() {
                     <h2 className="text-5xl font-bold mb-4">Design Revolucionário</h2>
                     <p className=" text-xl text-gray-400">Cada detalhe foi pensado para criar a melhor experiência.</p>
                 </div>
-            
-            <div className="grid grid-cols-2 gap-8 mb-16">
-                <div className="bg-gray-900 rounded-3xl p-8">
-                    <img className=" w-full rounded-2xl mb-4"src="../../public/img/titanium-design (1).jpg" alt="iphone-titanium"> </img>
-                    <h3 className="font-bold mb-2 text-3xl">Titânio Premium</h3>
-                    <p className="text-gray-300">Estrutura em titânio de grau aeroespacial. O smartphone mais forte e leve.</p>
+
+                <div className="grid grid-cols-2 gap-8 mb-16">
+                    <div className="bg-gray-900 rounded-3xl p-8">
+                        <img
+                            className="w-full rounded-2xl mb-4"
+                            src="/img/titanium-design (1).jpg"
+                            alt="iPhone em titânio"
+                        />
+                        <h3 className="font-bold mb-2 text-3xl">Titânio Premium</h3>
+                        <p className="text-gray-300">Estrutura em titânio de grau aeroespacial. O smartphone mais forte e leve.</p>
+                    </div>
+
+                    <div className="bg-gray-900 rounded-exl p-8"> 
+                        <img
+                            className=" w-full rounded-2xl mb-4"
+                            src="/img/ios-features (1).jpg"
+                            alt="ios 2025"
+                        />
+                        <h3 className="font-bold mb-2 text-3xl">IOS 26 </h3>
+                        <p className="text-gray-300"> O sistema operacional mais avançado do mundo com IA integrada. </p>
+                    </div>
                 </div>
 
+
                 <div>
-                    <img className=" w-full rounded-2xl mb-4" src="../../public/img/ios-features (1).jpg" alt="ios 2025"></img>
-                    <h3 className="font-bold mb-2 text-3xl"> IOS 26 </h3>
-                    <p className="text-gray-300"> O sistema operacional mais avançado do mundo com IA integrada. </p>
+                    <h3> A18 PRO </h3>
+                    <p> O chip mais poderoso em um smartphone </p>
+                    <img src="/img/chip-a18-pro (1).jpg" alt="chip a18"/>
                 </div>
-                </div>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    
+                </ul>
+                   
+                
             </div>
         </section>
 
